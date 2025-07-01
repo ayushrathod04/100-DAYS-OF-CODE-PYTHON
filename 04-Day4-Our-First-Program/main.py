5774bb726a7f926f11b7a25aa18b1fb2cf2d8295
Ayush Rathod <132492631+ayushrathod04@users.noreply.github.com>
2025-07-01 12:00:49 +0530

    practice code

---
+++ 04-Day4-Our-First-Program/main.py
@@ -0,0 +1,4 @@
+print("hello world, this is my first program in Python")
+print("I am excited to learn Python!")
+print ("I hope to become a great Python developer.")
+print("I will practice every day to improve my skills.")
