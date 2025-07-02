9dd1324e3f023d8a194e0f9b3a003720985984db
Ayush Rathod <132492631+ayushrathod04@users.noreply.github.com>
2025-07-02 12:52:12 +0530

    while loop

---
+++ 18-Day18-While-Loop/main.py
@@ -0,0 +1,10 @@
+i= int (input("Enter a number: "))
+while i < 10:
+    print("The number is less than 10")
+    i = int(input("Enter a number: "))
+    print("The number is greater than or equal to 10")
+print("The loop has ended")
+# This code will keep asking the user for a number until they enter a number that is 10 or greater.
+# If the user enters a number less than 10, it will print a message and ask
+# for a new number. Once a number 10 or greater is entered, it will print a message indicating that the loop has ended.
+# This is a simple example of using a while loop to repeatedly ask for input until a condition
