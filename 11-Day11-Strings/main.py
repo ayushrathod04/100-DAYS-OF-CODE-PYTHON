f1a0d5439519c38dc42a269932837eff5f8cfef3
Ayush Rathod <132492631+ayushrathod04@users.noreply.github.com>
2025-07-02 11:42:35 +0530

    strings

---
+++ 11-Day11-Strings/main.py
@@ -0,0 +1,16 @@
+name="Alex"
+friend="John"
+anthorfriend="Jane"
+apple = '''He said,"Hello, how are you?"
+I said,"I am fine, thank you!"'''
+print("Hello, " + name)
+# print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+#print(name[4])
+# print(name[5]) # Throws an error
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
